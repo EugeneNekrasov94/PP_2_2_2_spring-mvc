81f332abaed834e85924d91b601633db5e03a46a
EugeneNekrasov94 <enekrasov1994@gmail.com>
2022-10-28 13:50:13 +0300

    Add DI

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,20 +1,24 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-
 import java.util.List;
 
 @Controller
 public class CarsController {
+    private CarService service;
+    @Autowired
+    public CarsController(CarService service) {
+        this.service = service;
+    }
+
     @GetMapping(value = "/cars")
     public String printCarsInfo(@RequestParam("count") int count, ModelMap modelMap) {
-        CarService service = new CarServiceImpl();
         List<Car> cars = service.getCars(count);
         modelMap.addAttribute("cars", cars);
         return "cars";
