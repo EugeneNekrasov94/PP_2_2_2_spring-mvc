65b79f547e0fcf2a505be0a365bcd622327b0340
EugeneNekrasov94 <enekrasov1994@gmail.com>
2022-10-28 11:11:57 +0300

    Just commit

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -37,4 +37,13 @@ public class Car {
     public void setPrice(int price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,24 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl implements CarService {
+    private List<Car> cars = new ArrayList<>();
+
+    {
+        cars.add(new Car(1L, "BMW", 50000));
+        cars.add(new Car(2L, "Lexus", 40000));
+        cars.add(new Car(3L, "Lada", 20000));
+        cars.add(new Car(4L, "FAW", 25000));
+        cars.add(new Car(5L, "Ferrari", 100000));
+    }
+
+    @Override
+    public List<Car> getCars(int n) {
+        return cars.stream().filter(c -> cars.indexOf(c) < n).collect(Collectors.toList());
+    }
+}
